ddb2fac7e437f277317aeaf346fa7d93cc517275
nicole-shiftgig <ncarlson@shiftgig.com>
2017-06-27 08:02:35 -0500

    Add 404

--- parsing_science/__init__.py
+++ parsing_science/__init__.py
@@ -1,5 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
+
 
 app = Flask(__name__)
+
+
+@app.errorhandler(404)
+def not_found(error):
+  return render_template('404.html'), 404
 
 import parsing_science.views
